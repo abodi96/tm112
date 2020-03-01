07c8bece3f925d80bca1ff9ed11af866ea1b0f08
Abdulla <abodi96@gmail.com>
2020-03-01 17:14:08 +0300

    Drawing file added

---
+++ meeting 4/drawing.py
@@ -0,0 +1,24 @@
+#note that the file name cannot be same as the library imported
+# from turtle import *
+# forward(50)
+# right(90)
+# forward(50)
+# left(90)
+# forward(50)
+# input()
+
+import turtle
+
+# turtle.ht()
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+
+for i in range(10):
+    turtle.forward(10)
+
+
+
+input()
